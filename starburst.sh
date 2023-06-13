75ba5091e02a605b241cc6a59d9f44f46b69907f
Secondary Worlds <102990752+SecondaryWorlds@users.noreply.github.com>
2023-06-13 14:51:15 -0500

    Update starburst.sh

--- starburst.sh
+++ starburst.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
 set -e
-sudo pacman -Syu --noconfirm
+
+# Variables
+essential_packages=(
+  a52dec adobe-source-sans-pro-fonts android-tools aspell-enbase-devel avahi base-devel bluez ca-certificates cjson clang cmake config cuda curl doxygen ecryptfs-utils eigen enchant exfat-utils faac faad2 ffmpeg flac font freetype2 fuse-exfat git glfw-wayland glibc glslang glslang-tools gst-libav gst-plugins-good gstreamer hidapi hunspell-en_US icedtea-web jasper jdk jre8-open lame languagetool libbsd libdca libdv libdvdcss libdvdnav libdvdread libegl libeigen3 libglvnd libjpeg libmad libmpeg2 libmythes libopenxr-dev libopenxr-loader1 libopenxr-utils libopenxr1-monado libtheora libudev libusb libuvc libv41 libv4l libvorbis libvulkan libx11 libx11-xcb libxcb libxcursor libxext libxi libxinerama libxkbcommon-x11 libxrandr libxtst libxv libxxf86vm linux-firmware lsof mesa meson monado-cli monado-gui mythes-en ninja nlohmann-json nvidia opencv openssh openssl openxr openxr-layer-apidump openxr-layer-corevalidationpatch patch pkg-config pkgconf pkgstats python python-pip python-pipx python3 rsync sdl2 seatd systemd ttf-anonymous-pro ttf-bitstream-vera ttf-dejavu ttf-droid ttf-gentium ttf-liberation ttf-ubuntu-font-family ufw unzip v4l-utils vulkan-devel vulkan-headers vulkan-loader vulkan-tools wavpack wayland wayland-protocols wget x264 xcb-util xcb-util-cursor xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xcb-util-xrm xf86-video-amdgpu xr-hardware xvidcore
+)
+
+yay_packages=(
+  alacritty python2 python2-setuptools openxr-loader-git cmake libjpeg monado monado-git openhmd-git OpenCV Doxygen systemd-devel python OpenXR python3-pip libuvc ninja
+)
+
+pip_packages=(
+  libclang ffmpeg
+)
+
+# Function to install packages using yay
+install_packages_with_yay() {
+  yay -S --noconfirm --needed "${@}"
+}
+
+# Function to install packages using pip
+install_packages_with_pip() {
+  python -m venv myenv
+  source myenv/bin/activate
+  pip install "${@}"
+  deactivate
+}
+
+# Function to clone and build a repository
+clone_and_build_repository() {
+  repository_url=$1
+  repository_name=$2
+  git clone "$repository_url"
+  cd "$repository_name"
+  sudo meson build
+  sudo ninja -C build install -j24
+  cd ..
+  sudo rm -rf "$repository_name"
+}
+
 # Install essential packages
-sudo pacman -S --needed a52dec adobe-source-sans-pro-fonts android-tools aspell-enbase-devel avahi base-devel bluez ca-certificates cjson clang cmake config cuda curl doxygen ecryptfs-utils eigen enchant exfat-utils faac faad2 ffmpeg flac font freetype2 fuse-exfat git glfw-wayland glibc glslang glslang-tools gst-libav gst-plugins-good gstreamer hidapi hunspell-en_US icedtea-web jasper jdk jre8-open lame languagetool libbsd libdca libdv libdvdcss libdvdnav libdvdread libegl libeigen3 libglvnd libjpeg libmad libmpeg2 libmythes libopenxr-dev libopenxr-loader1 libopenxr-utils libopenxr1-monado libtheora libudev libusb libuvc libv41 libv4l libvorbis libvulkan libx11 libx11-xcb libxcb libxcursor libxext libxi libxinerama libxkbcommon-x11 libxrandr libxtst libxv libxxf86vm linux-firmware lsof mesa meson monado-cli monado-gui mythes-en ninja nlohmann-json nvidia opencv openssh openssl openxr openxr-layer-apidump openxr-layer-corevalidationpatch patch pkg-config pkgconf pkgstats python python-pip python-pipx python3 rsync sdl2 seatd systemd ttf-anonymous-pro ttf-bitstream-vera ttf-dejavu ttf-droid ttf-gentium ttf-liberation ttf-ubuntu-font-family ufw unzip v4l-utils vulkan-devel vulkan-headers vulkan-loader vulkan-tools wavpack wayland wayland-protocols wget x264 xcb-util xcb-util-cursor xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xcb-util-xrm xf86-video-amdgpu xr-hardware xvidcore
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed "${essential_packages[@]}"
 
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh 
@@ -17,14 +56,11 @@ makepkg -si --noconfirm
 cd ..
 sudo rm -rf yay-git
 
-# Install using yay
-yay -S --noconfirm --needed alacritty python2 python2-setuptools openxr-loader-git cmake libjpeg monado monado-git openhmd-git OpenCV Doxygen systemd-devel python OpenXR python3 python-pip libuvc ninja
+# Install packages using yay
+install_packages_with_yay "${yay_packages[@]}"
 
-# Install using pip
-python -m venv myenv
-source myenv/bin/activate
-pip install libclang ffmpeg 
-deactivate
+# Install packages using pip
+install_packages_with_pip "${pip_packages[@]}"
 
 # Run Supernova (Monado and OpenXR check)
 # sudo chmod +x /Stardust/supernova.sh
@@ -34,28 +70,26 @@ deactivate
 # Build and install flatbuffers v2.0.8 manually
 git clone --branch v2.0.8 https://github.com/google/flatbuffers.git
 cd flatbuffers
-sudo 
-sed -i 's/#include <string>/#include <string>\n#include <cstdint>/' tests/reflection_test.h
+sudo sed -i 's/#include <string>/#include <string>\n#include <cstdint>/' tests/reflection_test.h
 sudo mkdir build
 cd build
 sudo cmake ..
 sudo make
 sudo make install
 cd ../..
-sudo rm -r -f flatbuffers
+sudo rm -rf flatbuffers
 
 # Set the CUDAToolkit_ROOT environment variable
 export CUDAToolkit_ROOT=/usr/local/cuda
 
 # Install OpenXR-SDK
-clone https://github.com/KhronosGroup/OpenXR-SDK.git
+git clone https://github.com/KhronosGroup/OpenXR-SDK.git
 cd OpenXR-SDK
 sudo cmake . -G -DCMAKE_INSTALL_PREFIX=/usr -Bbuild
 sudo ninja -C build install
 cd ..
 
 # Install Monado
-sh
 git clone https://gitlab.freedesktop.org/monado/monado.git
 cd monado
 sudo meson build
@@ -70,7 +104,7 @@ sudo cmake --build build-server
 sudo mkdir -p ~/.config/openxr/1/
 sudo ln --relative --symbolic --force build-server/openxr_wivrn-dev.json ~/.config/openxr/1/active_runtime.json
 cd ..
-sudo rm -r -f WiVRn
+sudo rm -rf WiVRn
 
 # Setup Stardust
 git clone https://github.com/StardustXR/telescope.git
@@ -93,5 +127,4 @@ source "$HOME/.bashrc"
 
 # Display a message indicating completion
 echo "The PATH environment variable has been updated."
-
 echo "Installation and setup completed successfully."
