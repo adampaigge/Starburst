3ff1b301cd08a5142913dc6b765e7c5f7e8f4e12
Secondary Worlds <102990752+SecondaryWorlds@users.noreply.github.com>
2023-06-13 14:02:08 -0500

    Update starburst.sh

--- starburst.sh
+++ starburst.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Install essential packages
-sudo pacman -Syu --needed $(echo "a52dec meson glslang curl patch python pkgconf libusb hidapi wayland opencv sdl2 base-devel cmake vulkan-devel libx11-xcb xcb-util-keysyms xcb-util-wm xcb-util-image libxrandr mesa adobe-source-sans-pro-fonts android-tools aspell-enbase-devel avahi base-devel bluez ca-certificates cjson clang cmake cuda curl doxygen ecryptfs-utils eigen enchant exfat-utils faac faad2 ffmpeg flac font config freetype2 fuse-exfat git glfw-wayland glibc glslang glslang-tools gst-libav gst-plugins-good gstreamer hidapi hunspell-en_US icedtea-web jasper jre8-open jdk lame languagetool libbsd libdca libdv libdvdcss libdvdnav libdvdread libegl libeigen3 libglvnd libjpeg libmad libmpeg2 libmythes libopenxr-dev libopenxr-loader1 libopenxr-utils libopenxr1-monado libtheora libudev libusb libuvc libv41 libv4l libvorbis libvulkan libx11 libx11-xcb libxcb libxcursor libxext libxi libxinerama libxkbcommon-x11 libxrandr libxtst libxv libxxf86vm linux-firmware lsof mesa meson monado-cli monado-gui mythes-en ninja nlohmann-json nvidia opencv openssh openssl openxr openxr-layer-apidump openxr-layer-corevalidationpatch pkg-config pkgconf pkgstats python python-pip python-pipx python3 rsync sdl2 seatd systemd ttf-anonymous-pro ttf-bitstream-vera ttf-dejavu ttf-droid ttf-gentium ttf-liberation ttf-ubuntu-font-family ufw unzip v4l-utils vulkan-headers vulkan-loader vulkan-tools wavpack wayland wayland-protocols wget x264 xcb-util xcb-util-cursor xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xcb-util-xrm xf86-video-amdgpu xr-hardware xvidcore" | awk '!seen[$0]++')
+sudo pacman -Syu --needed a52dec adobe-source-sans-pro-fonts android-tools aspell-enbase-devel avahi base-devel bluez ca-certificates cjson clang cmake config cuda curl doxygen ecryptfs-utils eigen enchant exfat-utils faac faad2 ffmpeg flac font freetype2 fuse-exfat git glfw-wayland glibc glslang glslang-tools gst-libav gst-plugins-good gstreamer hidapi hunspell-en_US icedtea-web jasper jdk jre8-open lame languagetool libbsd libdca libdv libdvdcss libdvdnav libdvdread libegl libeigen3 libglvnd libjpeg libmad libmpeg2 libmythes libopenxr-dev libopenxr-loader1 libopenxr-utils libopenxr1-monado libtheora libudev libusb libuvc libv41 libv4l libvorbis libvulkan libx11 libx11-xcb libxcb libxcursor libxext libxi libxinerama libxkbcommon-x11 libxrandr libxtst libxv libxxf86vm linux-firmware lsof mesa meson monado-cli monado-gui mythes-en ninja nlohmann-json nvidia opencv openssh openssl openxr openxr-layer-apidump openxr-layer-corevalidationpatch patch pkg-config pkgconf pkgstats python python-pip python-pipx python3 rsync sdl2 seatd systemd ttf-anonymous-pro ttf-bitstream-vera ttf-dejavu ttf-droid ttf-gentium ttf-liberation ttf-ubuntu-font-family ufw unzip v4l-utils vulkan-devel vulkan-headers vulkan-loader vulkan-tools wavpack wayland wayland-protocols wget x264 xcb-util xcb-util-cursor xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xcb-util-xrm xf86-video-amdgpu xr-hardware xvidcore
 
+# Install Rust
 curl https://sh.rustup.rs -sSf | sh 
 source "$HOME/.cargo/env"
 
@@ -20,9 +21,10 @@ yay -S --needed alacritty python2 python2-setuptools openxr-loader-git cmake lib
 # Install using pip
 python -m venv myenv
 source myenv/bin/activate
-
 sudo pip install libclang ffmpeg 
 deactivate
+
+# Run Supernova (Monado and OpenXR check)
 
 # Build and install flatbuffers v2.0.8 manually
 sudo git clone --branch v2.0.8 https://github.com/google/flatbuffers.git
@@ -47,6 +49,7 @@ sudo cmake . -G -DCMAKE_INSTALL_PREFIX=/usr -Bbuild
 sudo ninja -C build install
 cd ..
 
+# Install Monado
 sh
 sudo git clone https://gitlab.freedesktop.org/monado/monado.git
 cd monado
@@ -54,41 +57,27 @@ sudo meson build
 sudo ninja -C build install -j24
 cd ..
 
-# Clone the WiVRn repository
+# Install WiVRn
 sudo git clone https://github.com/Meumeu/WiVRn.git
 cd WiVRn
-
-# Build and set up WiVRn
 sudo cmake -B build-server . -GNinja -DWIVRN_BUILD_CLIENT=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo
 sudo cmake --build build-server
-
-# Set WiVRn as the active OpenXR runtime
 sudo mkdir -p ~/.config/openxr/1/
 sudo ln --relative --symbolic --force build-server/openxr_wivrn-dev.json ~/.config/openxr/1/active_runtime.json
 cd ..
 sudo rm -r -f WiVRn
 
-# Clone the telescope repository
+# Setup Stardust
 sudo git clone https://github.com/StardustXR/telescope.git
 cd telescope
 
-# Take ownership of .sh scripts
 sudo chown -R $USER:$USER *.sh
 
-# Execute setup.sh
 sudo ./setup.sh
-
-# Execute hmd-setup.sh
 sudo ./hmd-setup.sh
-
-# Change into the server folder
 cd repos/server
-
-# Build and install the server
 sudo cargo build
 sudo cargo install --path .
-
-# Install flatland
 sudo cargo install flatland magnetar comet
 
 # Append the export statement to the .bashrc file
