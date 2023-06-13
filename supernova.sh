90436b2cde8e228e1db4b0c1aa2c2b9a1dde6e6b
Secondary Worlds <102990752+SecondaryWorlds@users.noreply.github.com>
2023-06-13 13:34:46 -0500

    Update supernova.sh

--- supernova.sh
+++ supernova.sh
@@ -1,107 +1,26 @@
 #!/bin/bash
 
-# Update system
-sudo pacman -Syu --noconfirm
+# Update the package manager
+sudo pacman -Syu
 
-# Install essential packages
-sudo pacman -S --noconfirm base-devel libglvnd freetype2 cmake curl fontconfig glfw-wayland glslang libjpeg libusb meson ninja nlohmann-json opencv pkgconf python-pip python-pipx python3 seatd vulkan-headers wget xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xf86-video-amdgpu mesa
+# Install required dependencies for Monado
+sudo pacman -S cmake meson ninja git pkgconf libxrandr libxinerama libxcursor libxi libxinerama libxxf86vm libxss libxdamage libxfixes libxcomposite libxrender libglvnd libnotify libpulse libusb hidapi libdrm libinput libunwind
 
-# Install yay package manager
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay
+# Install required dependencies for OpenXR
+sudo pacman -S vulkan-icd-loader
 
-# Install using yay
-yay -S --noconfirm --mflags --skipinteg --nocheck alacritty python2 python2-setuptools cmake doxygen libjpeg monado-git openhmd openxr-loader-git python systemd-devel
-
-# Install using pip
-pip install libclang ffmpeg
-
-# Build and install flatbuffers v2.0.8 manually
-git clone --branch v2.0.8 https://github.com/google/flatbuffers.git
-cd flatbuffers
-sed -i 's/#include <string>/#include <string>\n#include <cstdint>/' tests/reflection_test.h
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ../..
-rm -rf flatbuffers
-
-# Clone the Monado repository
+# Clone Monado repository
 git clone https://gitlab.freedesktop.org/monado/monado.git
-cmake -G Ninja -S monado -B build -DCMAKE_INSTALL_PREFIX=/usr
-ninja -C build install
-rm -rf monado
 
-# Set the CUDAToolkit_ROOT environment variable
-echo 'export CUDAToolkit_ROOT=/usr/local/cuda' >> "$HOME/.bashrc"
+# Enter Monado directory
+cd monado
 
-# Install OpenXR-SDK
-git clone https://github.com/KhronosGroup/OpenXR-SDK.git
-cd OpenXR-SDK
-cmake . -G Ninja -DCMAKE_INSTALL_PREFIX=/usr -B build
+# Build and install Monado
+meson build
+ninja -C build
 sudo ninja -C build install
-cd ..
-rm -rf OpenXR-SDK
 
-# Delete the WiVRn folder if it exists
-if [ -d "WiVRn" ]; then
-    rm -rf WiVRn
-fi
+# Install OpenXR runtime
+sudo ln -sf /usr/local/share/openxr/1.0.0/ /usr/share/openxr/loader/
 
-# Clone the WiVRn repository
-git clone https://github.com/Meumeu/WiVRn.git
-cd WiVRn
-
-# Build and set up WiVRn
-cmake -B build-server . -GNinja -DWIVRN_BUILD_CLIENT=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake --build build-server
-
-# Set WiVRn as the active OpenXR runtime
-mkdir -p ~/.config/openxr/1/
-ln -s -f $(realpath build-server/openxr_wivrn-dev.json) ~/.config/openxr/1/active_runtime.json
-cd ..
-rm -rf WiVRn
-
-# Clone the telescope repository
-git clone https://github.com/StardustXR/telescope.git
-cd telescope
-
-# Take ownership of .sh scripts
-sudo chown -R $USER:$USER *.sh
-
-# Execute setup.sh
-./setup.sh
-
-# Execute hmd-setup.sh
-./hmd-setup.sh
-
-# Change into the server folder
-cd repos/server
-
-# Build and install the server
-cargo build
-cargo install --path .
-
-# Return to the telescope folder
-cd ../..
-
-# Change into the flatland folder
-cd flatland
-
-# Install flatland
-cargo install flatland
-
-# Append the export statement to the .bashrc file
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> "$HOME/.bashrc"
-
-# Reload the .bashrc file in the current terminal session
-source "$HOME/.bashrc"
-
-# Display a message indicating completion
-echo "The PATH environment variable has been updated."
-echo "Installation and setup completed successfully."
+echo "Dependencies installed and Monado, OpenXR installed successfully!"
