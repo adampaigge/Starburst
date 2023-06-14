515c4ad54ab1afa26f534c17d7ab52d10d61ea13
Secondary Worlds <102990752+SecondaryWorlds@users.noreply.github.com>
2023-06-14 12:30:12 -0500

    Update starburst.sh

--- starburst.sh
+++ starburst.sh
@@ -22,7 +22,7 @@ sudo rm -rf yay-git
 # sudo pacman -R glfw-x11
 
 # Install packages using yay
-yay -S --noconfirm --needed alacritty python2 python2-setuptools openxr-loader-git cmake libjpeg monado monado-git openhmd-git OpenCV Doxygen systemd-devel python OpenXR python3 python-pip libuvc ninja
+yay -S --noconfirm --needed python2 python2-setuptools openxr-loader-git cmake libjpeg monado monado-git openhmd-git OpenCV Doxygen systemd-devel python OpenXR python3 python-pip libuvc ninja
 
 # Install using pip
 python -m venv myenv
